abad390088b605e501926fd974c5f0bd77e56775
Anaru Herbert <anaruherbert@gmail.com>
2019-11-26 21:17:42 +1000

    [#18] Added rounds to workout

--- FourCornersWatch/FourCornersWatch WatchKit Extension/ContentView.swift
+++ FourCornersWatch/FourCornersWatch WatchKit Extension/ContentView.swift
@@ -19,8 +19,8 @@ struct ContentView: View {
     @State private var intervalRestTimeIndex: Int = 0
     @State private var intervalIndex : Int = 0
     
-    let minimumIntervalRestTimeInSeconds = 3
-    let minimumIntervals = 10
+    let minimumIntervalRestTimeInSeconds = 4
+    let minimumIntervals = 5
     
     var intervalRestTimeValue : Int {
         return intervalRestTimeIndex + minimumIntervalRestTimeInSeconds
@@ -30,7 +30,26 @@ struct ContentView: View {
         return intervalIndex + minimumIntervals
     }
     
-     
+    //Set Round Values
+    @State private var roundTimer : Timer? = Timer()
+    @State private var roundRestTimeIndex : Int = 0
+    @State private var roundIndex : Int = 4
+    @State private var currentRoundCount : Int  = 1
+    
+    let minimumRoundRestTimeInSeconds = 10 // up to 59 second
+    let minimumRounds = 4 // go up to 12
+    
+    var roundRestTimeValue : Int {
+        return roundRestTimeIndex + minimumRoundRestTimeInSeconds
+    }
+    
+    var roundsValue : Int {
+        return roundIndex + minimumRounds
+    }
+    
+    
+    
+    
     let speech = AVSpeechSynthesizer()
     
     var body: some View {
@@ -39,31 +58,50 @@ struct ContentView: View {
                 Text(cornerLabel)
                     .font(.title)
                 Button(action: {
-                    self.stopIntervalTimer()
+                    self.stopWorkout()
                 }){
                     Text("Stop")
+                    
                 }
             } else {
                 
                 Form{
-                    Picker("Rest", selection: $intervalRestTimeIndex) {
-                        ForEach(minimumIntervalRestTimeInSeconds ..< 11) {
-                            Text("\($0) seconds")
+                    HStack {
+                        Picker("Intervals", selection: $intervalIndex) {
+                            ForEach(minimumIntervals ..< 21) {
+                                Text("\($0)")
+                            }
                         }
-                    }
-                    Picker("Intervals", selection: $intervalIndex) {
-                        ForEach(minimumIntervals ..< 21) {
-                            Text("\($0)")
+                        Picker("Rest", selection: $intervalRestTimeIndex) {
+                            ForEach(minimumIntervalRestTimeInSeconds ..< 11) {
+                                Text("\($0)s")
+                            }
                         }
                     }
                     
+                    HStack {
+                        Picker("Rounds", selection: $roundIndex) {
+                            ForEach(minimumRounds ..< 12) {
+                                Text("\($0)")
+                            }
+                        }
+                        Picker("Rest", selection: $roundRestTimeIndex) {
+                            ForEach(minimumRoundRestTimeInSeconds ..< 60) {
+                                Text("\($0)s")
+                            }
+                        }
+                    }
+                }
+                Group {
+                    Button(action: {
+                        self.startTimer(intervals: self.intervalValue, rounds: self.roundsValue, intervalRestTime: Double(self.intervalRestTimeValue), roundRestTime: Double(self.roundRestTimeValue))
+                        }){
+                        Text("Start")
+                    }
+
                     
                 }
-                Button(action: {
-                    self.startIntervalTimer(intervals: self.intervalValue, restTime: Double(self.intervalRestTimeValue))
-                }){
-                    Text("Start")
-                }
+                
                 
             }
             
@@ -80,42 +118,63 @@ struct ContentView: View {
         utterTextToSpeech(utteredText: chosenDirection)
     }
     
-    func startIntervalTimer(intervals: Int, restTime: Double) {
+    
+    func startTimer(intervals: Int, rounds: Int, intervalRestTime: Double, roundRestTime: Double) {
         guard self.intervalTimer == nil else { return }
         
-        // need to move to round timer.
         cornerLabel = "Ready!"
+        self.utterTextToSpeech(utteredText: cornerLabel)
+        
         workoutInProgess = true
         
         var startingIntervals = 0
-      
         
-        self.intervalTimer = Timer.scheduledTimer(withTimeInterval: restTime, repeats: true){
+        
+        self.intervalTimer = Timer.scheduledTimer(withTimeInterval: intervalRestTime, repeats: true){
             timer in
             if startingIntervals < intervals {
                 self.chooseAndSpeakRandomCorner()
                 startingIntervals += 1
             } else {
-                let finishMessage = "Done!"
-                self.cornerLabel = finishMessage
-                
-                self.utterTextToSpeech(utteredText: finishMessage)
-                
+                self.cornerLabel = "R\(self.currentRoundCount) Done"
+                self.utterTextToSpeech(utteredText: "Round \(self.currentRoundCount + 1) Done.")
                 self.stopIntervalTimer()
+                
+                if self.currentRoundCount < rounds {
+                    self.roundTimer = Timer.scheduledTimer(withTimeInterval: roundRestTime, repeats: false){
+                        timer in
+                        self.currentRoundCount += 1
+                        self.startTimer(intervals: self.intervalValue, rounds: self.roundsValue, intervalRestTime: Double(self.intervalRestTimeValue), roundRestTime: Double(self.roundRestTimeValue))
+                    }
+                } else {
+                    self.stopWorkout()
+                }
             }
         }
+    
     }
     
+    
+    
+    
     func stopIntervalTimer(){
-        
         speech.stopSpeaking(at: .word)
         intervalTimer?.invalidate()
         intervalTimer = Timer()
         
         // TODO: put workout in progress in stop Round Timer
-        
+    }
+    
+    func stopWorkout() {
         workoutInProgess = false
-      
+        
+        speech.stopSpeaking(at: .word)
+        
+        intervalTimer?.invalidate()
+        intervalTimer = Timer()
+        
+        roundTimer?.invalidate()
+        roundTimer = Timer()
     }
     
     
